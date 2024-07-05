c7aefe82033646dfe1a801ee8332ff97f368e61a
Necraatal <milan.zlamal.qa@gmail.com>
2024-07-05 22:31:59 +0200

    feat: setup terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "ami" {
+  description = "The AMI to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The instance type to use for the EC2 instance"
+  type        = string
+}
