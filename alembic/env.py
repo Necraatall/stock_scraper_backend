3e96f81b59ec9af9de669dfec627b06a21de2707
Necraatal <milan.zlamal.qa@gmail.com>
2024-07-07 01:14:21 +0200

    bug: minor updates, test files deletion

--- alembic/env.py
+++ alembic/env.py
@@ -19,8 +19,8 @@ fileConfig(config.config_file_name)
 target_metadata = Base.metadata
 
 # Get the URL from the environment variables
-DATABASE_URL = os.getenv('DATABASE_URL')
 ALEMBIC_URL = os.getenv('ALEMBIC_URL')
+
 # Set DATABASE_URL based on the TESTING environment variable
 if os.getenv('TESTING') == 'true':
     DATABASE_URL = os.getenv('TEST_DATABASE_URL')
--- tests/test_database.py
+++ tests/test_database.py
@@ -6,8 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Use TEST_DATABASE_URL for SQLite database
-DATABASE_URL = os.getenv('TEST_DATABASE_URL')
+# Use DATABASE_URL for Postgresql database
+DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
